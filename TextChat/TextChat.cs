0388ce2d0814082759bd9fe511e7e381349e1810
Maniac Devil Knuckles <theknuckles.ru@gmail.com>
2023-12-04 18:40:28 +0300

    Update to latest Qurre

--- TextChat/TextChat.cs
+++ TextChat/TextChat.cs
@@ -1,47 +1,103 @@
-﻿using System;
-using Qurre;
-using Qurre.API.Events;
+﻿using CommandSystem;
 using System.Collections.Generic;
-
+using Qurre.API;
+using Qurre.API.Attributes;
+using Qurre.Events;
 using Player = Qurre.API.Player;
+using System;
+using System.Linq;
+using Qurre.API.Addons;
+
 
 namespace TextChat
 {
-    public class TextChat : Plugin
+    [PluginInit("TextChat","Maniac Devil Knuckles","1.0.0")]
+    public class TextChat 
     {
-        public override string Developer => "KoToXleB#4663";
-        public override string Name => "TextChat";
-        public override Version Version => new Version(1, 0, 0);
-        public override int Priority => int.MinValue;
-        public override void Enable()
+        public static ChatCommand ChatCommand { get; private set; } 
+
+        [PluginEnable]
+        public void Enable()
         {
-            Qurre.Events.Server.SendingConsole += OnSendConsole;
-            Qurre.Events.Round.Start += OnRoundStart;
+            if (!Config.Load())
+            {
+                Log.Warn("TextChat is disabled");
+                return;
+            }
+            ChatCommand = new ChatCommand();
+            RemoteAdmin.QueryProcessor.DotCommandHandler.RegisterCommand(ChatCommand);           
         }
-        public override void Disable()
+
+        [PluginDisable]
+        public void Disable()
         {
-            Qurre.Events.Server.SendingConsole -= OnSendConsole;
-            Qurre.Events.Round.Start -= OnRoundStart;
+            RemoteAdmin.QueryProcessor.DotCommandHandler.UnregisterCommand(ChatCommand);
+            ChatCommand = null;
         }
+
+        [EventMethod(RoundEvents.Start)]
         public void OnRoundStart()
         {
+            if (!Config.IsEnabled) return;
+            foreach (Player player in Player.List)
+            {
+                player.Client.SendConsole(Config.SendMessageWhenRoundIsStarted, "red");
+            }
+        }
+    }
 
-            foreach (Player player in Player.List)
-            {
-                player.SendConsoleMessage("Вы можете отправлять сообщения через наш текстовый чат на кнопку [Ё].\n Разговаривайте друг с другом!", "red");
-            }
-        }
-        public void OnSendConsole(SendingConsoleEvent ev)
+    public static class Config
+    {
+        public static bool IsEnabled { get; private set; } = true;
+
+        public static string SendMessageWhenRoundIsStarted { get; private set; } = "Вы можете отправлять сообщения через наш текстовый чат на кнопку [Ё].\n Разговаривайте друг с другом!";
+
+        public static string NameOfCommand { get; private set; } = "chat";
+
+        private static readonly JsonConfig jsonConfig = new JsonConfig("TextChat");
+
+        internal static bool Load()
         {
-            foreach (Player player in Player.List)
-            {
-                if (!player.IsHost)
-                {
-                    player.SendConsoleMessage($"{ev.Player.Nickname}: {ev.Message}", $"{Color[UnityEngine.Random.Range(0, Color.Count)]}");
-                    ev.Allowed = false;
-                }
-            }
+            IsEnabled = jsonConfig.SafeGetValue("IsEnabled", IsEnabled);
+            SendMessageWhenRoundIsStarted = jsonConfig.SafeGetValue("SendMessageWhenRoundIsStarted", SendMessageWhenRoundIsStarted);
+            NameOfCommand = jsonConfig.SafeGetValue("NameOfCommand", NameOfCommand);
+            return IsEnabled;
         }
+    }
+
+    [CommandHandler(typeof(ClientCommandHandler))]
+    public class ChatCommand : ICommand
+    {
+        public string Command => Config.NameOfCommand;
+
+        public string[] Aliases => new string[] { };
+
+        public string Description => "Chatting with players";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            response = "You did not write";
+            Player ply = Extensions.GetPlayer(sender as CommandSender);
+            if (arguments.Count == 0 || ply.IsHost|| ply == null) return false;
+            if (!ply.RoleInfomation.IsAlive)
+            {
+                foreach (Player player in Player.List.Where(p=>!p.RoleInfomation.IsAlive))
+                    if (!player.IsHost) player.Client.SendConsole($"{ply.UserInfomation.Nickname}: {string.Join(" ", arguments)}", $"{Color[UnityEngine.Random.Range(0, Color.Count)]}");
+            }
+            else if (ply.RoleInfomation.IsScp)
+            {
+                foreach (Player player in Player.List.Where(p => p.RoleInfomation.IsScp))
+                    if (!player.IsHost) player.Client.SendConsole($"{ply.UserInfomation.Nickname}: {string.Join(" ", arguments)}", $"{Color[UnityEngine.Random.Range(0, Color.Count)]}");
+            }
+            else
+            {
+                foreach (Player player in Player.List.Where(p => p.DistanceTo(ply) <= 5f))
+                    if (!player.IsHost) player.Client.SendConsole($"{ply.UserInfomation.Nickname}: {string.Join(" ", arguments)}", $"{Color[UnityEngine.Random.Range(0, Color.Count)]}");
+            }
+            response = "Sended";
+            return true;
+        }
+
         public List<string> Color = new List<string>()
         {
             "red",
@@ -52,4 +108,5 @@ namespace TextChat
             "white"
         };
     }
+
 }
